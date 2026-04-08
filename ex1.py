fd598f7651b74da3bd095411c91f4081a9964a5f
veronezeyuri <113545941+veronezeyuri@users.noreply.github.com>
2026-04-08 18:04:31 +0000

    boa

---
+++ ex1.py
@@ -0,0 +1,9 @@
+# Recebendo dois dados do usuário
+dado1 = input("Digite o primeiro dado: ")
+dado2 = input("Digite o segundo dado: ")
+
+# Concatenando os dados
+resultado = dado1 + " " + dado2
+
+# Exibindo o resultado
+print("Resultado concatenado:", resultado)
